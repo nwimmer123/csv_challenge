ab7891d253d7de89b03f4c063436e300d9ec58a9
Noah Wimmer <nwimmer123@yahoo.com>
2016-01-14 09:57:12 -0800

    formats are good

--- csv.rb
+++ csv.rb
@@ -1,10 +1,11 @@
 require 'csv'
 require 'pry'
+
 class Human 
 
 	attr_accessor :last_name, :first_name, :middle_initial, :gender, :favorite_color, :date_of_birth
 
-	def initialize(last_name, first_name, *middle_initial, gender, favorite_color, date_of_birth)
+	def initialize(last_name, first_name, middle_initial, gender, favorite_color, date_of_birth)
 		@last_name = last_name
 		@first_name = first_name
 		@middle_initial = middle_initial
@@ -19,16 +20,19 @@ end
 records = []
 
 CSV.foreach("sample/comma.txt") do |row|
-  row.insert(2, "")
+  row = row.join.split
+  row.insert(2)
 	records << row
+
 end
 CSV.foreach("sample/pipe.txt") do |row|
-  row = row.join.split
+  row = row.join.gsub("-", "/").split
   row.delete_if {|element| element == "|"}
 	records << row
 end
 CSV.foreach("sample/space.txt") do |row|
-  row = row.join.split
+  row = row.join.gsub("-", "/").split
+  row[5], row[4] = row[4], row[5]
   records << row
 end
 
@@ -44,37 +48,8 @@ records.each do |element|
   new_records << h
 end
 
+new_records
+
 p new_records
 
-
-
-
-
-# records.each do |row|
-# 	row = row.join.split
-# 	row.delete_if {|element| element == "|"}
-# 	new_records << row
-# end
-
-# records = records[0].join.split
-# records.delete_if {|element| element == "|"}
-
-
-
-# p records
-
-# CSV.foreach("sample/space.txt") do |row|
-# 	records << row
-# end
-
-
-# array = CSV.read("sample/comma.txt")
-# array2 = CSV.read("sample/pipe.txt")
-# array3 = CSV.read("sample/space.txt")
-
-# p array
-# p ""
-# p array2
-# p ""
-# p array3
 
